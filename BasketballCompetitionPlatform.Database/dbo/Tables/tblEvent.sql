f8dbccca6249eb8273cea50d824256a1a5eaeee7
Nemanja <nemanjaplavsic.net@yahoo.com>
2022-12-18 11:19:40 +0100

    Create Database with all relationships between tables #1

--- BasketballCompetitionPlatform.Database/dbo/Tables/tblEvent.sql
+++ BasketballCompetitionPlatform.Database/dbo/Tables/tblEvent.sql
@@ -4,7 +4,7 @@
     [Place] NVARCHAR(50) NOT NULL, 
     [NumberOfTeamsInEvent] INT NOT NULL, 
     [DateTimeStart] DATETIME NOT NULL, 
-    [ DateTimeEnd] DATETIME NOT NULL, 
-    [ NumberOfMatchesUntilTheEnd] INT NOT NULL, 
+    [DateTimeEnd] DATETIME NOT NULL, 
+    [NumberOfMatchesUntilTheEnd] INT NOT NULL, 
     [IsEventFinished] BIT NOT NULL DEFAULT 0
 )
--- BasketballCompetitionPlatform.Database/dbo/Tables/tblGame.sql
+++ BasketballCompetitionPlatform.Database/dbo/Tables/tblGame.sql
@@ -3,6 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Time] TIME NOT NULL, 
     [Date] DATE NOT NULL, 
-    [ LocationOfTheBasketballCourt] NVARCHAR(50) NOT NULL, 
+    [LocationOfTheBasketballCourt] NVARCHAR(50) NOT NULL, 
     [IsGameFinished] BIT NOT NULL DEFAULT 0
 )
